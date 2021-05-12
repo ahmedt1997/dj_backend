4769892c1c543c594a84ab084081d8f7356d63f9
Ahmed Touzani <ahmedbbccaa@gmail.com>
2021-05-12 06:33:22 +0000

    step 1 is create plugin.js and then create .env file to setup the configuration from cloudinary

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7a811f4df1b80042c2d840aa25806904'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0d13dc19-7d6b-43ea-8a5c-65e352186a93'
+};
